5631d45320c5d311cebb84ec495102da8ed00d5d
Jaybit0 <jannik.lindemann3@gmail.com>
2024-02-03 12:56:10 +0100

    Updated tests

--- Tests/GroverCircuitBuilder.jl
+++ Tests/GroverCircuitBuilder.jl
@@ -28,6 +28,9 @@ configureYaoPlots()
 using Yao
 using Yao.EasyBuild, YaoPlots
 using Test
+using Logging
+
+Base.global_logger(ConsoleLogger(stdout, Logging.Warn))
 
 @testset "Main functions" begin
     circ = empty_circuit(2, 2)
@@ -69,7 +72,43 @@ end
 end
 
 @testset "Full examples" begin
-    
+    @test begin
+        grover_circ = empty_circuit(1, 4)
+
+        hadamard(grover_circ, 2:5)
+
+        rotation(grover_circ, model_lanes(grover_circ)[1], control_lanes = 2:5; max_rotation_rad = 2*pi)
+
+        criterion = [true]
+        out, main_circ, grov, oracle_function = auto_compute(grover_circ, criterion; ignore_errors = false)
+        true
+    end
+
+    @test begin
+        # Initialize an empty circuit with 2 target lanes and 4 model lanes
+        grover_circ = empty_circuit(2, 4)
+
+        # Apply Hadamard Gates on the lanes 3 -> 6
+        hadamard(grover_circ, 3:6)
+
+        # Apply 3 controlled rotations on the first lane with a granularity of pi/4 (max_rotation_rad / 2^length(control_lanes))
+        block, meta = learned_rotation(grover_circ, 1, 3:5)
+        meta.data["batch"] = 1
+        meta.data["lane"] = 1
+        meta.manipulator(block, meta, inv) = meta.data["batch"] == 1
+        # Apply 1 controlled rotation on the second lane with a granularity of pi (max_rotation_rad / 2^length(control_lanes))
+        learned_rotation(grover_circ, 2, 6)
+
+        # Apply a controlled negation to the second lane
+        not(grover_circ, 2; control_lanes = 1)
+
+        # We expect the first lane to return true and the second lane to return false
+        # As we use multiple target lanes, auto_compute automatically inserts a lane below the target lanes which encode the criterions to this lane
+        # The reflection is done with respect to the inserted lane
+        criterion = [true, true]
+        out, main_circ, grov, oracle_function = auto_compute(grover_circ, criterion; evaluate = true, ignore_errors = false)
+        true
+    end
 end
 
 @testset "Helper functions" begin
