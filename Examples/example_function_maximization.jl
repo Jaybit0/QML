7765aab47e5b381ed3a20b64bace9664e0d46f65
Jaybit0 <jannik.lindemann3@gmail.com>
2024-05-25 13:23:21 +0200

    example function maximization

---
+++ Examples/example_function_maximization.jl
@@ -0,0 +1,32 @@
+# ======== IMPORTS ========
+# =========================
+
+if !isdefined(Main, :QML)
+    include("../Modules/QML.jl")
+    using .QML
+end
+
+using Yao
+using Yao.EasyBuild, YaoPlots
+
+# ========== CODE ==========
+# ==========================
+
+# Set to true to augment the circuit by rescaling with l := 0.75 and repeating measurements 3 times
+augment = false
+
+
+maxval = augment ? π*0.75 : π
+batch = augment ? [[true], [true], [true]] : [[true]]
+
+model_lanes = 1
+param_lanes = 2:5
+
+# Initialize a circuit with 2 model and 2 parameter lanes
+circuit = chain(5, repeat(H, param_lanes), control(2, 1 => Ry(maxval / 2)), control(3, 1 => Ry(maxval / 4)), control(4, 1 => Ry(maxval / 8)), control(5, 1 => Ry(maxval / 16)))
+
+grover = QMLBlock(circuit, model_lanes, param_lanes, batch; log=true)
+
+#vizcircuit(grover.compiled_circuit.main_circuit)
+
+plotmeasure(grover; sort=true, num_entries=10)
