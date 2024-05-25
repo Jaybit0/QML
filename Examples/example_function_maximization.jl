9cb93aede67e5ee73057cf183b1299fb63a82bc1
Jaybit0 <jannik.lindemann3@gmail.com>
2024-05-25 13:28:46 +0200

    Changed comment

--- Examples/example_function_maximization.jl
+++ Examples/example_function_maximization.jl
@@ -22,7 +22,7 @@ batch = augment ? [[true], [true], [true]] : [[true]]
 model_lanes = 1
 param_lanes = 2:5
 
-# Initialize a circuit with 2 model and 2 parameter lanes
+# Initialize the circuit
 circuit = chain(5, repeat(H, param_lanes), control(2, 1 => Ry(maxval / 2)), control(3, 1 => Ry(maxval / 4)), control(4, 1 => Ry(maxval / 8)), control(5, 1 => Ry(maxval / 16)))
 
 grover = QMLBlock(circuit, model_lanes, param_lanes, batch; log=true)
