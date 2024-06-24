8fba0b930825edb1c90cacdaa79e458fe214eb52
Melody Lee <melody.lee357@gmail.com>
2024-06-24 14:07:53 +0200

    functioning OAA run functioN

--- Examples/cascading_example1.jl
+++ Examples/cascading_example1.jl
@@ -8,12 +8,12 @@ using Yao.EasyBuild, YaoPlots
 num_model_lanes = 2
 rotation_precision = 1
 
-ma = create_OAACircuit(num_model_lanes, rotation_precision)
+model = create_OAACircuit(num_model_lanes, rotation_precision)
 
-vizcircuit(ma.architecture)
+# vizcircuit(model.architecture)
 
-Φ = zero_state(2) |> chain(2, put(1=>Ry(π)))
-run_OAA(ma, Φ)
+state = zero_state(2) |> chain(2, put(1=>Ry(π)))
 
+run_OAA(model, state)
 
-
+    
