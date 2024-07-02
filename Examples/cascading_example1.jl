fa22f5c4db0d56d156e01c4a6f432e4b97ae5edf
Melody Lee <melody.lee357@gmail.com>
2024-07-02 12:23:46 +0200

    fixed cnot mapping

--- Examples/cascading_example1.jl
+++ Examples/cascading_example1.jl
@@ -5,17 +5,17 @@ include("../Modules/OAACircuitBuilder.jl")
 using Yao
 using Yao.EasyBuild, YaoPlots
 
-num_model_lanes = 2
+# num_model_lanes = 2
 rotation_precision = 1
 
-model = create_oaa_circuit(num_model_lanes, rotation_precision)
+training_data = [[0,0],[1,0], [1,1]]
+
+model = create_oaa_circuit(training_data, rotation_precision)
 
 vizcircuit(model.architecture)
 
-state = zero_state(2) |> chain(2, put(1=>Ry(π)))
+# state = zero_state(2) |> chain(2, put(1=>Ry(π)))
 
-run_OAA(model, state)
+# run_OAA(model, state)
 
-plotmeasure(model)
-
-    
+# plotmeasure(model)
